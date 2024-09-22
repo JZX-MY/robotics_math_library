3178cb729d34ce568bb2b805791e70a47320ca2d
jingzongxin <571188579@qq.com>
2024-09-22 16:04:55 +0800

    first Verson

--- src/main.cpp
+++ src/main.cpp
@@ -17,15 +17,15 @@ int main() {
     double theta = 2.22033;
     Point2D translation = { -2.11988, 6.8836 };
 
-    Point2D P1_r = RoboticsMath::getAfterRotation(P1_w, translation, theta);
-    Point2D P2_r = RoboticsMath::getAfterRotation(P2_w, translation, theta);
+    Point2D P1_r = RoboticsMath::getAfterRotation2D(P1_w, translation, theta);
+    Point2D P2_r = RoboticsMath::getAfterRotation2D(P2_w, translation, theta);
 
     // 输出求转换后 P1 和 P2 坐标
     cout << "P1 在新坐标系（机器人坐标系）下的坐标: (" << P1_r.x << ", " << P1_r.y << ")" << endl;
     cout << "P2 在新坐标系（机器人坐标系）下的坐标: (" << P2_r.x << ", " << P2_r.y << ")" << endl;
 
-    Point2D P1_w2 = RoboticsMath::getBeforeRotation(P1_r, translation, theta);
-    Point2D P2_w2 = RoboticsMath::getBeforeRotation(P2_r, translation, theta);
+    Point2D P1_w2 = RoboticsMath::getBeforeRotation2D(P1_r, translation, theta);
+    Point2D P2_w2 = RoboticsMath::getBeforeRotation2D(P2_r, translation, theta);
 
     // 输出求转换前 P1 和 P2 坐标
     cout << "P1 在原坐标系（世界坐标系）下的坐标: (" << P1_w2.x << ", " << P1_w2.y << ")" << endl;
@@ -45,15 +45,15 @@ int main() {
     Point3D translation = { -2.11988, 6.8836 ,0.3};
     string axis = "Z";
 
-    Point3D P1_r = RoboticsMath::getAfterRotation(P1_w, translation, theta, axis);
-    Point3D P2_r = RoboticsMath::getAfterRotation(P2_w, translation, theta, axis);
+    Point3D P1_r = RoboticsMath::getAfterRotation3D(P1_w, translation, theta, axis);
+    Point3D P2_r = RoboticsMath::getAfterRotation3D(P2_w, translation, theta, axis);
 
     // 输出求转换后 P1 和 P2 坐标
     cout << "P1 在新坐标系（机器人坐标系）下的坐标: (" << P1_r.x << ", " << P1_r.y << ", " << P1_r.z << ")" << endl;
     cout << "P2 在新坐标系（机器人坐标系）下的坐标: (" << P2_r.x << ", " << P2_r.y << ", " << P2_r.z << ")" << endl;
 
-    Point3D P1_w2 = RoboticsMath::getBeforeRotation(P1_r, translation, theta, axis);
-    Point3D P2_w2 = RoboticsMath::getBeforeRotation(P2_r, translation, theta, axis);
+    Point3D P1_w2 = RoboticsMath::getBeforeRotation3D(P1_r, translation, theta, axis);
+    Point3D P2_w2 = RoboticsMath::getBeforeRotation3D(P2_r, translation, theta, axis);
 
     // 输出求转换前 P1 和 P2 坐标
     cout << "P1 在原坐标系（世界坐标系）下的坐标: (" << P1_w2.x << ", " << P1_w2.y << ", " << P1_w2.z << ")" << endl;
